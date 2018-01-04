5a17009c8c631c3a73d5a8170eb9ea7b0473035e
Alex Morel <alex.morel@irealite.com>
2018-01-04 11:37:42 +0100

    [Android] Exercice 4 : refactor MainActivity by using ButterKnife

--- app/src/main/java/org/miage/placesearcher/MainActivity.java
+++ app/src/main/java/org/miage/placesearcher/MainActivity.java
@@ -1,35 +1,25 @@
 package org.miage.placesearcher;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.RatingBar;
-import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private RatingBar mRatingBar;
-    private TextView mTextView;
+    @BindView(R.id.ratingBar) RatingBar mRatingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Get the rating bar from layout file
-        this.mRatingBar = (RatingBar) this.findViewById(R.id.ratingBar);
-
-        // Get the textview from layout file
-        this.mTextView = (TextView) this.findViewById(R.id.textView);
-
-        // Define onClickListener on the text view : decrease rating at each click
-        mTextView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                MainActivity.this.mRatingBar.setRating(MainActivity.this.mRatingBar.getRating() - 1);
-            }
-        });
+        // Binding ButterKnife annotations now that content view has been set
+        ButterKnife.bind(this);
     }
 
     @Override
@@ -39,5 +29,11 @@ public class MainActivity extends AppCompatActivity {
 
         // Increment rating bar's rating
         this.mRatingBar.setRating(this.mRatingBar.getRating() + 1);
+    }
+
+    @OnClick(R.id.textView)
+    public void clickedOnTextField() {
+        // Decrease rating at each click on TextField
+        this.mRatingBar.setRating(this.mRatingBar.getRating() - 1);
     }
 }
