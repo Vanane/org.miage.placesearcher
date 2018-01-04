19622418217ce33d3c310e9120e94237989ce9b0
Alex Morel <alex.morel@irealite.com>
2018-01-04 15:33:03 +0100

    [Android] Exercice 6 : RecyclerView with StringAdapter and 50k items

--- app/src/main/java/org/miage/placesearcher/MainActivity.java
+++ app/src/main/java/org/miage/placesearcher/MainActivity.java
@@ -1,17 +1,23 @@
 package org.miage.placesearcher;
 
 import android.os.Bundle;
-import android.widget.RatingBar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.miage.placesearcher.ui.StringAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.ratingBar) RatingBar mRatingBar;
+    @BindView(R.id.recyclerView)
+    RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,20 +26,19 @@ public class MainActivity extends AppCompatActivity {
 
         // Binding ButterKnife annotations now that content view has been set
         ButterKnife.bind(this);
+
+        List<String> listItems = new ArrayList<String>();
+        for (int i = 0; i < 50000; i ++) {
+            listItems.add("Item" + (i + 1));
+        }
+        StringAdapter adapter = new StringAdapter(this, listItems);
+        mRecyclerView.setAdapter(adapter);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
     @Override
     protected void onResume() {
         // Do NOT forget to call super.onResume()
         super.onResume();
-
-        // Increment rating bar's rating
-        this.mRatingBar.setRating(this.mRatingBar.getRating() + 1);
-    }
-
-    @OnClick(R.id.textView)
-    public void clickedOnTextField() {
-        // Decrease rating at each click on TextField
-        this.mRatingBar.setRating(this.mRatingBar.getRating() - 1);
     }
 }
