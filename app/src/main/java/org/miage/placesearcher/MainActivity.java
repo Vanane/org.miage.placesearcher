13362f184ecda98ce1bb8f20e99515e6391c2594
Alex Morel <alex.morel@irealite.com>
2018-01-04 11:13:08 +0100

    [Android] Exercice 2 - Define OnClickListener on TextView

--- app/src/main/java/org/miage/placesearcher/MainActivity.java
+++ app/src/main/java/org/miage/placesearcher/MainActivity.java
@@ -1,13 +1,16 @@
 package org.miage.placesearcher;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.RatingBar;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     private RatingBar mRatingBar;
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +19,17 @@ public class MainActivity extends AppCompatActivity {
 
         // Get the rating bar from layout file
         this.mRatingBar = (RatingBar) this.findViewById(R.id.ratingBar);
+
+        // Get the textview from layout file
+        this.mTextView = (TextView) this.findViewById(R.id.textView);
+
+        // Define onClickListener on the text view : decrease rating at each click
+        mTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MainActivity.this.mRatingBar.setRating(MainActivity.this.mRatingBar.getRating() - 1);
+            }
+        });
     }
 
     @Override
