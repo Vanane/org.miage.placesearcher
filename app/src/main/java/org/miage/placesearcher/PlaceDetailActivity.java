5a7ee3e2ec671d34c685bbd25eecf5d71ef480eb
Alex Morel <alex.morel@irealite.com>
2018-01-05 10:33:01 +0100

    [Android] Exercice 11 : add button allowing to pick a picture and display it in the Place Details Activity

--- app/src/main/java/org/miage/placesearcher/PlaceDetailActivity.java
+++ app/src/main/java/org/miage/placesearcher/PlaceDetailActivity.java
@@ -1,11 +1,17 @@
 package org.miage.placesearcher;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -17,8 +23,14 @@ import butterknife.OnClick;
 
 public class PlaceDetailActivity extends AppCompatActivity {
 
+    private static final int SELECT_PHOTO = 42;
+
     @BindView(R.id.activity_detail_place_street)
     TextView mPlaceStreet;
+
+    @BindView(R.id.activity_detail_place_pic)
+    ImageView mPlacePic;
+
     private String mPlaceStreetValue;
 
     @Override
@@ -52,5 +64,38 @@ public class PlaceDetailActivity extends AppCompatActivity {
         sendIntent.putExtra(Intent.EXTRA_TEXT, "J'ai découvert " + mPlaceStreetValue + " grâce à Place Searcher !");
         sendIntent.setType("text/plain");
         startActivity(sendIntent);
+    }
+
+    @OnClick(R.id.activity_detail_button_galery)
+    public void clickedOnPickFromGalery() {
+        // Open galery picker using an Intent
+        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+        photoPickerIntent.setType("image/*");
+        startActivityForResult(photoPickerIntent, SELECT_PHOTO);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+
+        // If we get a result from the SELECT_PHOTO query
+        switch(requestCode) {
+            case SELECT_PHOTO:
+                if(resultCode == RESULT_OK){
+                    // Get the selected image as bitmap
+                    Uri selectedImage = imageReturnedIntent.getData();
+                    InputStream imageStream = null;
+                    try {
+                        imageStream = getContentResolver().openInputStream(selectedImage);
+                        Bitmap selectedImageBitmap = BitmapFactory.decodeStream(imageStream);
+
+                        // Set the bitmap to the picture
+                        mPlacePic.setImageBitmap(selectedImageBitmap);
+                    } catch (FileNotFoundException e) {
+                        // Silent catch : image will not be displayed
+                    }
+
+                }
+        }
     }
 }
