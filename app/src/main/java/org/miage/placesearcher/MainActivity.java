5a6c91422a37747cd1961565ade6fb5fd32fe72a
Alex Morel <alex.morel@irealite.com>
2018-01-05 09:40:55 +0100

    [Android] Exercice 9 : add Place details Activity

--- app/src/main/java/org/miage/placesearcher/MainActivity.java
+++ app/src/main/java/org/miage/placesearcher/MainActivity.java
@@ -1,5 +1,6 @@
 package org.miage.placesearcher;
 
+
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
--- app/src/main/java/org/miage/placesearcher/PlaceDetailActivity.java
+++ app/src/main/java/org/miage/placesearcher/PlaceDetailActivity.java
@@ -1,8 +1,34 @@
 package org.miage.placesearcher;
 
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 /**
  * Created by alexmorel on 04/01/2018.
  */
 
-class PlaceDetailActivity {
+public class PlaceDetailActivity extends AppCompatActivity {
+
+    @BindView(R.id.activity_detail_place_street)
+    TextView mPlaceStreet;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        ButterKnife.bind(this);
+        mPlaceStreet.setText("Détails du lieu");
+    }
+
+    @OnClick(R.id.activity_detail_place_street)
+    public void clickedOnPlaceStreet() {
+        finish();
+    }
 }
