5b048815756454a759477dc7995a887130f1195d
Alex Morel <alex.morel@irealite.com>
2018-01-24 21:38:49 +0100

    [Android] Exercice 16 - Step 5 : performance improvement : naive way

--- app/src/main/java/org/miage/placesearcher/PlaceSearchService.java
+++ app/src/main/java/org/miage/placesearcher/PlaceSearchService.java
@@ -24,8 +24,10 @@ import retrofit2.http.Query;
 
 public class PlaceSearchService {
 
+    private static final long REFRESH_DELAY = 2000;
     public static PlaceSearchService INSTANCE = new PlaceSearchService();
     private final PlaceSearchRESTService mPlaceSearchRESTService;
+    private long mLastUpdateTime;
 
     private PlaceSearchService() {
         // Create GSON Converter that will be used to convert from JSON to Java
@@ -48,26 +50,31 @@ public class PlaceSearchService {
         mPlaceSearchRESTService = retrofit.create(PlaceSearchRESTService.class);
     }
 
-    public void searchPlacesFromAddress(final String search) {
-        // Call to the REST service
-        mPlaceSearchRESTService.searchForPlaces(search).enqueue(new Callback<PlaceSearchResult>() {
-            @Override
-            public void onResponse(Call<PlaceSearchResult> call, retrofit2.Response<PlaceSearchResult> response) {
-                // Post an event so that listening activities can update their UI
-                if (response.body() != null && response.body().features != null) {
-                    EventBusManager.BUS.post(new SearchResultEvent(response.body().features));
-                } else {
-                    // Null result
+    public void searchPlacesFromAddress(final String search, boolean force) {
+        // First check if we already made a call recently (and if this is the case, do nothing)
+        if (force || System.currentTimeMillis() - mLastUpdateTime > REFRESH_DELAY){
+            mLastUpdateTime = System.currentTimeMillis();
+
+            // Call to the REST service
+            mPlaceSearchRESTService.searchForPlaces(search).enqueue(new Callback<PlaceSearchResult>() {
+                @Override
+                public void onResponse(Call<PlaceSearchResult> call, retrofit2.Response<PlaceSearchResult> response) {
+                    // Post an event so that listening activities can update their UI
+                    if (response.body() != null && response.body().features != null) {
+                        EventBusManager.BUS.post(new SearchResultEvent(response.body().features));
+                    } else {
+                        // Null result
+                        // We may want to display a warning to user (e.g. Toast)
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<PlaceSearchResult> call, Throwable t) {
+                    // Request has failed or is not at expected format
                     // We may want to display a warning to user (e.g. Toast)
                 }
-            }
-
-            @Override
-            public void onFailure(Call<PlaceSearchResult> call, Throwable t) {
-                // Request has failed or is not at expected format
-                // We may want to display a warning to user (e.g. Toast)
-            }
-        });
+            });
+        }
     }
 
     // Service describing the REST APIs
