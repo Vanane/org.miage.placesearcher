b7508efcf277d9478898c0e76d574b8221410fae
Alex Morel <alex.morel@irealite.com>
2018-01-24 21:21:44 +0100

    [Android] Exercice 16 - Step 2 : add markers to map

--- app/src/main/java/org/miage/placesearcher/model/PlaceProperties.java
+++ app/src/main/java/org/miage/placesearcher/model/PlaceProperties.java
@@ -3,15 +3,19 @@ package org.miage.placesearcher.model;
 import com.google.gson.annotations.Expose;
 
 public class PlaceProperties {
-        @Expose
-        public String name;
+    @Expose
+    public String name;
 
-        @Expose
-        public String postcode;
+    @Expose
+    public String postcode;
 
-        @Expose
-        public String city;
+    @Expose
+    public String city;
 
-        @Expose
-        public String type;
+    @Expose
+    public String type;
+
+    public boolean isStreet() {
+        return type != null && type.equals("street");
+    }
 }
